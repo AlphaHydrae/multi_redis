0faac0623c5445ee69d2c7aaae1796f17abbe193
Simon Oulevay <simon.oulevay@gmail.com>
2014-01-12 14:13:52 +0100

    Moved specs.

--- spec/multi_redis/version_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'helper'
-
-describe "Version" do
-
-  it "should be correct" do
-    expect(MultiRedis::VERSION).to eq(File.read(File.join(File.dirname(__FILE__), '..', '..', 'VERSION')))
-  end
-end
---
+++ spec/version_spec.rb
@@ -0,0 +1,8 @@
+require 'helper'
+
+describe "Version" do
+
+  it "should be correct" do
+    expect(MultiRedis::VERSION).to eq(File.read(File.join(File.dirname(__FILE__), '..', 'VERSION')))
+  end
+end
